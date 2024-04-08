b65bc751c8e8f962ec3404bc9065c10e06fbc4bd
marcantoinem <marc-antoine.m@outlook.com>
2024-04-08 01:30:36 -0400

    Add classroom schedule page

--- aep-schedule-website/src/frontend/pages/apropos.rs
+++ aep-schedule-website/src/frontend/pages/apropos.rs
@@ -1,6 +1,18 @@
 use leptos::*;
 
 #[component]
-fn APropos() -> impl IntoView {
-    view! {}
+pub fn HomePage() -> impl IntoView {
+    view! {
+        <section class="home">
+            <h1>"Générateur d'horaire de l'AEP v2"</h1>
+            <p>
+                "Vous en avez assez d'avoir des horaires horribles fait pas le registrariat? Vous êtes au bon endroit. Le générateur d'horaire v2 est une réécriture plus performante, intuive et moderne du générateur d'horaire. Il vous aidera à trouver un horaire parfait."
+            </p>
+            <h2>"Auteurs"</h2>
+            <ul>
+                <li>"Marc-Antoine Manningham - Création du frontend et du backend entièrement en Rust"</li>
+                <li>"Raphael Salvas, Achille Saint-Hillier, Sunnee Chevalier et Gabriel Billard - Inspiration de leur TP3 de LOG2420 pour le design du générateur"</li>
+            </ul>
+        </section>
+    }
 }
