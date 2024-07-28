7c433d59cf0c766a43905ee0bdfd0a251eff803c
marcantoinem <marc-antoine.m@outlook.com>
2024-07-28 00:42:41 -0400

    (bug) fix personal grid phantom selection

--- aep-schedule-website/src/frontend/components/options/personal.rs
+++ aep-schedule-website/src/frontend/components/options/personal.rs
@@ -40,9 +40,9 @@ where
         for day in initial_x..=destination_x {
             week[day].update(|v| {
                 if is_positive {
-                    *v |= 2u64.pow(destination_y as u32 + 1) - 2u64.pow(initial_y as u32);
+                    *v |= 2u64.pow(destination_y as u32 + 2) - 2u64.pow(initial_y as u32);
                 } else {
-                    *v &= !(2u64.pow(destination_y as u32 + 1) - 2u64.pow(initial_y as u32));
+                    *v &= !(2u64.pow(destination_y as u32 + 2) - 2u64.pow(initial_y as u32));
                 }
             });
         }
@@ -80,14 +80,14 @@ where
                     };
                     view! {
                         <div style=style class=class
-                        on:pointerdown=move |e| {
-                            set_initial.set(Some((i, j)));
-                            set_positive.set((week[i].get() & (1 << j)) == 0);
-                            let _ = e.target().unwrap().dyn_ref::<Element>().unwrap().release_pointer_capture(e.pointer_id());
-                        }
-                        on:pointerover=move |_| {
-                            set_destination.set((i, j));
-                        }>
+                            on:pointerdown=move |e| {
+                                set_initial.set(Some((i, j)));
+                                set_positive.set((week[i].get() & (1 << j)) == 0);
+                                let _ = e.target().unwrap().dyn_ref::<Element>().unwrap().release_pointer_capture(e.pointer_id());
+                            }
+                            on:pointerover=move |_| {
+                                set_destination.set((i, j));
+                            }>
                         </div>
                     }
                 }).collect_view()
